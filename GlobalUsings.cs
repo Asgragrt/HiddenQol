a04ede7534acc764c270cc49366ca32e3e25a8be
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-07-27 20:36:26 -0600

    🚚 Fixed namespace

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,8 +1,8 @@
+global using static HiddenQol.Properties.MelonBuildInfo;
+global using static HiddenQol.QoLManager;
+global using static HiddenQol.Save;
 global using Il2Cpp;
 global using Il2CppAssets.Scripts.UI.Panels;
 global using MelonLoader;
 global using Tomlet;
 global using UnityEngine;
-global using static HiddenQol.MelonBuildInfo;
-global using static HiddenQol.QoLManager;
-global using static HiddenQol.Save;
