517460511febb385998b5090194b38df92a51e5a
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2025-05-10 20:43:58 -0600

    🚀 Version bump

--- Properties/MelonBuildInfo.cs
+++ Properties/MelonBuildInfo.cs
@@ -3,10 +3,7 @@ namespace HiddenQol.Properties;
 internal static class MelonBuildInfo
 {
     internal const string Author = "RobotLucca, lxy & Asgragrt";
-
     internal const string Description = "";
-
-    internal const string ModVersion = "2.1.0";
-
+    internal const string ModVersion = "2.2.0";
     internal const string Name = "HiddenQol";
 }
